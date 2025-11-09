1ec4a3f6f59b1a2ad5ffc1434c07b87c402e1ddd
Jhonatan Nascimento <jhonatanfnm@gmail.com>
2025-11-09 16:02:28 -0300

    alterando url da api

--- src/service/api.ts
+++ src/service/api.ts
@@ -1,10 +1,6 @@
 import axios from "axios";
-import { Platform } from "react-native";
 
-const API_URL =
-  Platform.OS === "android"
-    ? process.env.EXPO_PUBLIC_API_URL_ANDROID
-    : process.env.EXPO_PUBLIC_API_URL_LOCAL;
+const API_URL = process.env.EXPO_PUBLIC_API_URL_LOCAL;
 
 const api = axios.create({
   baseURL: API_URL,
